5ca6bfd233695ff8987949e779af912382940304
Kirill <ykirillm@mail.ru>
2024-06-06 02:04:18 +0300

    eslint and prettier and tsconfig

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,23 +1,21 @@
 import React, {useState} from "react";
 
-export const App = () => {
-    const [count, setCount] = useState(0)
+export const App = (): React.JSX.Element => {
+  const [count, setCount] = useState(0);
 
-    const handlePlus = () => {
-        setCount((prev) => prev + 1)
-    }
+  const handlePlus = (): void => {
+    setCount(prev => prev + 1);
+  };
 
-    const handleMinus = () => {
-        setCount((prev) => prev - 1)
-    }
+  const handleMinus = (): void => {
+    setCount(prev => prev - 1);
+  };
 
-    return (
-        <div>
-            <button onClick={handlePlus}>plus</button>
-            <button onClick={handleMinus}>minus</button>
-            <span>{count}</span>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={handlePlus}>plus</button>
+      <button onClick={handleMinus}>minus</button>
+      <span>{count}</span>
+    </div>
+  );
 };
-
-
